2842b7c25d704545da46b3be92702a5e769b6050
david <dlondono@vertical.com.co>
2022-07-21 01:18:36 -0500

    updated

---
+++ src/data/clases.js
@@ -0,0 +1,296 @@
+export let all_courses = [
+  {
+    name: "Tecnocine",
+    code: "3010259",
+    credits: 3,
+    group: 1,
+    virtual: false,
+    clases: [
+      {
+        day: 2,
+        class_room: "aula 46-114 BLOQUE 46",
+        start_hour: 14,
+        end_hour: 18,
+      }
+    ]
+  },
+  {
+    name: "Plan B",
+    code: "30074420",
+    credits: 3,
+    group: 1,
+    virtual: true,
+    clases: [
+      {
+        day: 2,
+        class_room: "aula 46-110 BLOQUE 04",
+        start_hour: 16,
+        end_hour: 18,
+      },
+      {
+        day: 4,
+        class_room: "aula 46-110 BLOQUE 04",
+        start_hour: 16,
+        end_hour: 18,
+      },
+    ]
+  },
+  {
+    name: "Introducción a la creación de videojuegos",
+    code: "3010585",
+    credits: 3,
+    group: 1,
+    virtual: true,
+    clases: [
+      {
+        day: 2,
+        class_room: "M8-202",
+        start_hour: 14,
+        end_hour: 16,
+      },
+      {
+        day: 4,
+        class_room: "M8-202",
+        start_hour: 14,
+        end_hour: 16,
+      },
+    ]
+  },
+  {
+    name: "Emprendimiento",
+    code: "3010196",
+    credits: 4,
+    group: 1,
+    virtual: false,
+    clases: [
+      {
+        day: 3,
+        class_room: "04-108",
+        start_hour: 16,
+        end_hour: 18,
+      },
+      {
+        day: 5,
+        class_room: "04-108",
+        start_hour: 16,
+        end_hour: 18,
+      },
+    ]
+  },
+  {
+    name: "De la U a la O",
+    code: "3009433",
+    credits: 3,
+    group: 1,
+    virtual: false,
+    clases: [
+      {
+        day: 3,
+        class_room: "04-206",
+        start_hour: 14,
+        end_hour: 16,
+      },
+      {
+        day: 5,
+        class_room: "04-206",
+        start_hour: 14,
+        end_hour: 16,
+      },
+    ]
+  },
+  {
+    name: "De la U a la O",
+    code: "3009433",
+    credits: 3,
+    group: 2,
+    virtual: false,
+    clases: [
+      {
+        day: 3,
+        class_room: "04-206",
+        start_hour: 12,
+        end_hour: 14,
+      },
+      {
+        day: 5,
+        class_room: "04-206",
+        start_hour: 12,
+        end_hour: 14,
+      },
+    ]
+  },
+  {
+    name: "De la U a la O",
+    code: "3009433",
+    credits: 3,
+    group: 3,
+    virtual: false,
+    clases: [
+      {
+        day: 3,
+        class_room: "M3-224",
+        start_hour: 16,
+        end_hour: 18,
+      },
+      {
+        day: 4,
+        class_room: "M1-212",
+        start_hour: 16,
+        end_hour: 18,
+      },
+    ]
+  },
+  {
+    name: "De la U a la O",
+    code: "3009433",
+    credits: 3,
+    group: 4,
+    virtual: false,
+    clases: [
+      {
+        day: 3,
+        class_room: "M3-224",
+        start_hour: 12,
+        end_hour: 14,
+      },
+      {
+        day: 4,
+        class_room: "M3-224",
+        start_hour: 12,
+        end_hour: 14,
+      },
+    ]
+  },
+  {
+    name: "Paz en Colombia",
+    code: "3009693",
+    credits: 3,
+    group: 1,
+    virtual: false,
+    clases: [
+      {
+        day: 3,
+        class_room: "41-201",
+        start_hour: 8,
+        end_hour: 12,
+      },
+    ]
+  },
+  {
+    name: "Cátedra Universitaria",
+    code: "30096367",
+    credits: 3,
+    group: 1,
+    virtual: false,
+    clases: [
+      {
+        day: 1,
+        class_room: "24-416",
+        start_hour: 14,
+        end_hour: 18,
+      },
+    ]
+  },
+  {
+    name: "Cátedra Felicidad",
+    code: "3010712",
+    credits: 2,
+    group: 1,
+    virtual: false,
+    clases: [
+      {
+        day: 1,
+        class_room: "12-101",
+        start_hour: 8,
+        end_hour: 10,
+      },
+    ]
+  },
+  {
+    name: "Cátedra Felicidad",
+    code: "3010712",
+    credits: 2,
+    group: 2,
+    virtual: false,
+    clases: [
+      {
+        day: 1,
+        class_room: "12-101",
+        start_hour: 10,
+        end_hour: 12,
+      },
+    ]
+  },
+  {
+    name: "Cátedra Pedro Nel",
+    code: "3011025",
+    credits: 3,
+    group: 1,
+    virtual: false,
+    clases: [
+      {
+        day: 2,
+        class_room: "41-201",
+        start_hour: 16,
+        end_hour: 18,
+      },
+      {
+        day: 4,
+        class_room: "12-301",
+        start_hour: 18,
+        end_hour: 20,
+      },
+    ]
+  },
+  {
+    name: "Cátedra transformación digital",
+    code: "1000122-M",
+    credits: 3,
+    group: 1,
+    virtual: false,
+    clases: [
+      {
+        day: 3,
+        class_room: "",
+        start_hour: 7,
+        end_hour: 10,
+      },
+    ]
+  },
+  {
+    name: "Cátedra Ingeniería civil",
+    code: "3009065",
+    credits: 3,
+    group: 1,
+    virtual: false,
+    clases: [
+      {
+        day: 1,
+        class_room: "12-301",
+        start_hour: 8,
+        end_hour: 12,
+      },
+    ]
+  },
+  {
+    name: "Cátedra Gerardo Botero",
+    code: "3009016",
+    credits: 3,
+    group: 1,
+    virtual: false,
+    clases: [
+      {
+        day: 2,
+        class_room: "12-304",
+        start_hour: 16,
+        end_hour: 18,
+      },
+      {
+        day: 4,
+        class_room: "12-304",
+        start_hour: 16,
+        end_hour: 18,
+      },
+    ]
+  },
+]
---
+++ src/helpers/createShedule.js
@@ -0,0 +1,169 @@
+import { all_courses as cours } from "src/data/clases.js"
+
+let all_courses = cours.filter(cour => cour.clases.every(c => [2,3,4,5,6].includes(c.day)))
+
+export function getColor(code, group){
+  const colors = [
+    "rgb(228 113 93)",
+    "rgb(231 146 40)",
+    "rgb(231 191 16)",
+    "rgb(230 204 1)",
+    "rgb(143 230 33)",
+    "rgb(108 140 228)",
+    "rgb(146 139 230)",
+    "rgb(171 132 232)",
+    "rgb(211 121 229)",
+    "rgb(223 114 162)",
+    "rgb(230 113 113)",
+    "rgb(54 202 108)",
+    "rgb(32 191 133)",
+    "rgb(25 192 171)",
+    "rgb(14 191 218)",
+    "rgb(36 169 228)",
+    "rgb(76 145 230)",
+  ]
+  let index = all_courses.findIndex(
+    course => course.code === code && course.group === group
+  )
+  let color_index = index%colors.length
+  return colors[color_index]
+}
+
+function day_clases(day, course_clases){
+  let day_clases = []
+  for (const course of course_clases){
+    let course_clases = course["clases"].filter(
+      clas => clas.day === day
+    )
+    for (const clas of course_clases){
+      let complete_clas = {
+        ...clas,
+        ...course
+      }
+      delete complete_clas.clases
+      day_clases.push(complete_clas)
+    }
+  }
+  day_clases = day_clases.sort((a, b) => {
+    return a.start_hour - b.start_hour
+  })
+  return day_clases
+}
+
+function hoursOverlap(class1, class2){
+  if(class2.start_hour>class1.start_hour && class2.start_hour<class1.end_hour){
+    return true
+  }
+
+  if(class2.end_hour>class1.start_hour && class2.end_hour<class1.end_hour){
+    return true
+  }
+
+  if (class2.end_hour===class1.end_hour && class2.start_hour===class1.start_hour){
+    return true
+  }
+
+  return false
+}
+
+function clasesOverlap(class1, class2){
+  if (class1.day !== class2.day){
+    return false
+  }
+
+  if(hoursOverlap(class1, class2) || hoursOverlap(class2, class1)){
+    return true
+  }
+
+  return false
+}
+
+function coursesOverlap(courses1, courses2){
+  let clases1 = courses1.clases
+  let clases2 = courses2.clases
+
+  for (const class1 of clases1){
+    for (const class2 of clases2){
+      if(clasesOverlap(class1, class2)){
+        return true
+      }
+    }
+  }
+  return false
+}
+
+function coursesOverlapAll(allCourses, course){
+  for(const c of allCourses){
+    if (coursesOverlap(c, course)){
+      return true
+    }
+  }
+  return false
+}
+
+function sameCourse(course1, course2){
+  return course1.code == course2.code && course1.group == course2.group
+}
+
+function sameClases(clases1, clases2){
+  if(clases1.length !== clases2.length){
+    return false
+  }
+
+  for(const clas1 of clases1){
+    let index = clases2.findIndex(
+      clas2 => sameCourse(clas1, clas2)
+    )
+
+    if (index === -1){
+      return false
+    }
+  }
+  return true
+}
+
+function removeRepeated(nonOverlapingCourses){
+  return nonOverlapingCourses.filter(
+    (clases, index) => nonOverlapingCourses.findIndex( clasess => sameClases(clases, clasess)) === index
+  )
+}
+
+function getNonOverlapingCourses(courses){
+  if(courses.length === 0){
+    return []
+  }
+
+  let nonOverlapingClases = []
+  let currentNonOverlapingClases = []
+
+  for (let i = 0; i<courses.length; i++){
+    let overlap = false
+    for (let j=0; j<currentNonOverlapingClases.length; j++){
+      if(coursesOverlap(courses[i], currentNonOverlapingClases[j])){
+        let index = courses.findIndex(
+          course => sameCourse(course,currentNonOverlapingClases[j])
+        )
+        let newCourses = [...courses.slice(0,index), ...courses.slice(index+1)]
+        nonOverlapingClases.push(
+          ...getNonOverlapingCourses(newCourses)
+        )
+        overlap = true
+        break
+      }
+    }
+    if(overlap){
+      continue
+    }
+    currentNonOverlapingClases.push(courses[i])
+  }
+
+  if (currentNonOverlapingClases.length > 0){
+    nonOverlapingClases.push(currentNonOverlapingClases)
+  }
+
+  nonOverlapingClases = removeRepeated(nonOverlapingClases)
+
+  return nonOverlapingClases
+}
+
+export let nonOverlapingCourses = getNonOverlapingCourses([...all_courses])
